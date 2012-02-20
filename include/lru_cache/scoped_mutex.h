ca9981e8d9ad4d3fe854b9102783d36712072a77
PawelMarc <pwl@software.com.pl>
2012-02-20 22:22:18 +0100

    update

--- include/lru_cache/scoped_mutex.h
+++ include/lru_cache/scoped_mutex.h
@@ -4,36 +4,31 @@
 
 class scoped_lock
 {
+
+   public:
+
+   //Constructs and locks
+   explicit scoped_lock(pthread_mutex_t* m)
+      : mutex(m), locked(false) 
+   {  
+        pthread_mutex_lock(mutex);   
+        locked = true;  
+   }
+
+   //Destroys and unlocks
+   ~scoped_lock()
+   {
+      if(locked && mutex)   
+        pthread_mutex_unlock(mutex);
+   }
+
    private:
    //Non-copyable
    scoped_lock(scoped_lock const&);
    //Non-assignable
    scoped_lock& operator=  (scoped_lock const&);
 
-   public:
-
-   //Constructs the scoped_lock and locks the mutex using lock()
-   explicit scoped_lock(pthread_mutex_t* m)
-      : mp_mutex(m), m_locked(false) 
-   {  
-        pthread_mutex_lock(mp_mutex);   
-        m_locked = true;  
-   }
-
-   //Destroys the scoped_lock. If the mutex was not released and
-   //the mutex is locked, unlocks it using unlock() function
-   ~scoped_lock()
-   {
-      try{  if(m_locked && mp_mutex)   pthread_mutex_unlock(mp_mutex);  }
-      catch(...){}
-   }
-
-   //Return true if the mutex is locked
-   bool locked() const
-   {  return m_locked;  }
-
-   private:
-   pthread_mutex_t *mp_mutex; 
-   bool        m_locked;
+   pthread_mutex_t *mutex; 
+   bool  locked;
 };
 
